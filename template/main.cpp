467ad54cea27d820e340ea0e54b82c24f7b0c01a
lovezah <zah0109@126.com>
2025-09-14 17:31:28 +0800

    a

--- template/main.cpp
+++ template/main.cpp
@@ -1,11 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
+
+#ifdef LOCAL
+#include "debug.h"
+#else
+#define debug(...) 42
+#endif
+
 #define all(v) (v).begin(), (v).end()
 #define sz(v) ((int)(v).size())
-const char nl = '\n';
+using ll = long long;
+using ii = pair<int, int>;
+
 int main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(nullptr);
+  cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+  cout << setprecision(20);
   ${0}
 }
