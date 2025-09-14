ccf838bcec7e9b348aad42039f6c4e2d27ce1e41
lovezah <zah0109@126.com>
2025-09-14 16:36:37 +0800

    rm useless file

--- a.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define all(v) (v).begin(), (v).end()
-#define sz(v) ((int)(v).size())
-const char nl = '\n';
-int main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(nullptr);
-}
